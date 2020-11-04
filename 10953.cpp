6480d64f280734bbff85400e9dd26429a56b0d99
rnrn213 <usan213@gmail.com>
2020-11-04 10:34:30 +0900

    10953번 A+B - 6

---
+++ 10953.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+		int A, B;
+		char comma;
+		cin >> A >> comma >> B;
+		cout << A + B << '\n';
+	}
+}
