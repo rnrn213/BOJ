079706eceb3f4af6336484e7dd37eb302e93c033
rnrn213 <usan213@gmail.com>
2020-11-05 13:03:00 +0900

    1717번 집합의 표현

--- 1717.cpp
+++ 1717.cpp
@@ -2,59 +2,38 @@
 
 using namespace std;
 
+int Find(int arr[], int num) {
+	if (arr[num] == num) return num;
+	arr[num] = Find(arr, arr[num]);
+	return arr[num];
+}
+
+void Union(int arr[], int a, int b) {
+	a = Find(arr, a);
+	b = Find(arr, b);
+	if (a < b) arr[b] = a;
+	else arr[a] = b;
+}
+
 int main() {
 	int n, m;
-	cin >> n >> m;
-	set<int>** sets = new set<int>*[n + 1](NULL);   //º¤ÅÍ ½áº¸±â 
-	/*for (int i = 0; i <= n; i++) {
-		sets[i] = NULL;
-	}*/
+	scanf("%d %d", &n, &m);
 	
+	int* arr = new int[n + 1];
+	for (int i = 0; i < n + 1; i++) {
+		arr[i] = i;
+	}
+	
+	int order, a, b;
 	for (int i = 0; i < m; i++) {
-		int order, a, b;
-		cin >> order >> a >> b;
+		scanf("%d %d %d", &order, &a, &b);
 		
 		if (order == 0) {
-			if (sets[a] == NULL && sets[b] == NULL) {
-				set<int>* s = new set<int>;
-				s->insert(a);
-				s->insert(b);
-				sets[a] = sets[b] = s;
-			}
-			else if (sets[a] == NULL) {
-				sets[b]->insert(a);
-				sets[a] = sets[b];
-			}
-			else if (sets[b] == NULL) {
-				sets[a]->insert(b);
-				sets[b] = sets[a];
-			}
-			else {
-				/* set<int>* s = new set<int>;
-				merge(sets[a]->begin(), sets[a]->end(),
-						sets[b]->begin(), sets[b]->end(),
-						inserter(*s, s->begin()));
-				set<int>* pa = sets[a]; */
-				if (sets[a]->size() > sets[b]->size()) {
-					set<int>* pb = sets[b];
-					sets[a]->insert(sets[b]->begin(), sets[b]->end());
-					sets[b] = sets[a];
-					delete pb;
-				}
-				else {
-					set<int>* pa = sets[a];
-					sets[b]->insert(sets[a]->begin(), sets[a]->end());
-					sets[a] = sets[b];
-					delete pa;
-				}
-				/*set<int>* pb = sets[b];
-				delete pa, pb;*/
-			}
+			Union(arr, a, b);
 		}
-		else if (order == 1) {
-			if (sets[a] == NULL || sets[b] == NULL) cout << "NO\n";
-			else if (sets[a] == sets[b]) cout << "YES\n";
-			else cout << "NO\n";
+		else {
+			if (Find(arr, a) == Find(arr, b)) printf("YES\n");
+			else printf("NO\n");
 		}
 	}
 }
