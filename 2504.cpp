15f66a76768676bcf8edb7600e70a6a40328acbe
rnrn213 <73531200+rnrn213@users.noreply.github.com>
2020-11-09 22:55:23 +0900

    2504번 괄호의 값(재귀 함수 구현중)

--- 2504.cpp
+++ 2504.cpp
@@ -8,8 +8,21 @@ int getVal(char* c, int a, int b) {
 		else return 3;
 	}
 	else {
-		if (c[a] == '(' && c[b] == ')') return 2 * getVal(c, a + 1, b - 1);
-		else return 3 * getVal(c, a + 1, b - 1);
+		stack<int> s;
+		int start = a + 1;
+		int total = 0;
+		for (int i = a + 1; i < b; i++) {
+			if (c[i] == '(' || c[i] == '[') {
+				s.push(1);
+			}
+			else if (c[i] == ')' || c[i] == ']') {
+				s.pop();
+			}
+			if (s.empty()) {
+				total += getVal(c, start, i);
+				if (i != b - 1) start = i + 1;
+			}
+		}
 	}
 }
 
@@ -18,7 +31,18 @@ int main() {
 	char* c = new char[50];
 	fgets(c, sizeof(char)*50, stdin);
 	cout << strlen(c) << '\n';
+	int total = 0, start = 0;
 	for (int i = 0; i< strlen(c); i++) {
+		if (c[i] == '(' || c[i] == '[') {
+			s.push(1);
+		}
+		else if (c[i] == ')' || c[i] == ']') {
+				s.pop();
+		}
+		if (s.empty()) {
+			total += getVal(c, start, i);
+			if (i != strlen(c) - 1) start = i + 1;
+		}	
 	}
-	cout << c[0] << " " << c[strlen(c) - 2] << " strlen(c) - 1: " << c[strlen(c) - 1];
+	cout << total;
 }
