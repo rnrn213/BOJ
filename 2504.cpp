0d41820ff7de24cfac36ccd7b0c75a930f0d51cb
rnrn213 <usan213@gmail.com>
2020-11-10 09:54:58 +0900

    2504번 괄호의 값

--- 2504.cpp
+++ 2504.cpp
@@ -2,47 +2,86 @@
 
 using namespace std;
 
+bool isValid(char* c) {
+	bool v = true;
+	stack<int> s;
+	for (int i = 0; i < strlen(c); i++) {
+		if (c[i] == '(') {
+			s.push(1);
+		}
+		else if (c[i] == '[') {
+			s.push(2);
+		}
+		else if (c[i] == ')') {
+			if (s.empty()) {
+				v = false;
+				break;
+			}
+			else if (s.top() != 1) {
+				v = false;
+				break;
+			}
+			else {
+				s.pop();
+			}
+		}
+		else if (c[i] == ']') {
+			if (s.empty()) {
+				v = false;
+				break;
+			}
+			else if (s.top() != 2) {
+				v = false;
+				break;
+			}
+			else {
+				s.pop();
+			}
+		}
+	}
+	return v;
+}
+
 int getVal(char* c, int a, int b) {
+	int total = 0;
 	if (b - a == 1) {
-		if (c[a] == '(' && c[b] == ')') return 2;
-		else return 3;
+		return 1;
 	}
 	else {
 		stack<int> s;
 		int start = a + 1;
-		int total = 0;
 		for (int i = a + 1; i < b; i++) {
 			if (c[i] == '(' || c[i] == '[') {
 				s.push(1);
 			}
-			else if (c[i] == ')' || c[i] == ']') {
+			else if (c[i] == ')') {
 				s.pop();
+				if (s.empty()) {
+					total += 2 * getVal(c, start, i);
+					start = i + 1;
+				}
 			}
-			if (s.empty()) {
-				total += getVal(c, start, i);
-				if (i != b - 1) start = i + 1;
+			else if (c[i] == ']') {
+				s.pop();
+				if (s.empty()) {
+					total += 3 * getVal(c, start, i);
+					start = i + 1;
+				}
 			}
 		}
 	}
+	return total;
 }
 
 int main() {
 	stack<int> s;
 	char* c = new char[50];
 	fgets(c, sizeof(char)*50, stdin);
-	cout << strlen(c) << '\n';
-	int total = 0, start = 0;
-	for (int i = 0; i< strlen(c); i++) {
-		if (c[i] == '(' || c[i] == '[') {
-			s.push(1);
-		}
-		else if (c[i] == ')' || c[i] == ']') {
-				s.pop();
-		}
-		if (s.empty()) {
-			total += getVal(c, start, i);
-			if (i != strlen(c) - 1) start = i + 1;
-		}	
+	if (isValid(c)) {
+		int total = getVal(c, -1, strlen(c) - 1);
+		printf("%d", total);
 	}
-	cout << total;
+	else {
+		printf("0");
+	}
 }
