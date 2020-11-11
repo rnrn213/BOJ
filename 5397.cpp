1ad7b2a584cddeb436124afafdbd82e213711c6b
rnrn213 <73531200+rnrn213@users.noreply.github.com>
2020-11-11 21:41:50 +0900

    5397번 키로거

--- 5397.cpp
+++ 5397.cpp
@@ -4,15 +4,29 @@ using namespace std;
 
 void getPassword(const char* const keylogger, list<char>& password) {
 	list<char>::iterator cursor = password.begin();
-	for (int i = 0; i < strlen(keylogger); i++) {
+	int length = strlen(keylogger);
+	for (int i = 0; i < length; i++) {
 		if (keylogger[i] == '<') {
 			if (cursor != password.begin()) cursor--;
 		}
 		else if (keylogger[i] == '>') {
-			if (cursor != password.end() - 1) cursor++;
+			if (cursor != password.end()) cursor++;
 		}
 		else if (keylogger[i] == '-') {
-			
+			if (cursor != password.begin()) {
+				cursor--;
+				cursor = password.erase(cursor);
+			}
+		}
+		else {
+			if (cursor == password.end()) {
+				password.push_back(keylogger[i]);
+				cursor = password.end();
+			}
+			else {
+				cursor = password.insert(cursor, keylogger[i]);
+				cursor++;
+			}
 		}
 	}
 }
@@ -23,13 +37,18 @@ int main() {
 	
 	for (int i = 0; i < T; i++) {
 		char* keylogger = new char[1000001];
-		fgets(keylogger, sizeof(char)*1000001, stdin);
-		for (int j = 0; j < strlen(keylogger); j++) {
+		scanf("%s", keylogger);
+		//fgets(keylogger, sizeof(char)*1000001, stdin);
+		/*for (int j = 0; j < strlen(keylogger); j++) {
 			printf("%c", keylogger[j]);
 		}
-		printf("\n");
+		printf("\n");*/
 		
 		list<char> password;
-		
+		getPassword(keylogger, password);
+		for (list<char>::iterator it = password.begin(); it != password.end(); ++it) {
+			printf("%c", *it);
+		}
+		printf("\n");
 	}
 }
