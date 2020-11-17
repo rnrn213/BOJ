1ff6616f05df05b64063fbae5c952b2cac8690b1
rnrn213 <usan213@gmail.com>
2020-11-17 17:07:27 +0900

    1699번 제곱수의 합

--- 1699.cpp
+++ 1699.cpp
@@ -2,18 +2,37 @@
 
 using namespace std;
 
+int arr[100001];
+
+void init() {
+	arr[0] = 0;
+	arr[1] = 1;
+	
+	for (int i = 2; i <= 100000; i++) {
+		int min = 100001;
+		for (int j = sqrt(i); j > 0; j--) {
+			if (j * j < i) {
+				if (arr[i - j * j] < min) {
+					min = 1 + arr[i - j * j];
+				}
+			}
+			else {
+				min = 1;
+				break;
+			}
+		}
+		arr[i] = min;
+	}
+}
+
 int main() {
 	cin.tie(NULL);
 	ios::sync_with_stdio(false);
+
+	init();
 	
 	int N;
 	cin >> N;
 	
-	int count = 0;
-	while (N != 0) {
-		cout << N << " - " << pow(static_cast<int>(sqrt(N)), 2) << '\n';
-		N -= pow(static_cast<int>(sqrt(N)), 2);
-		count++;
-	}
-	cout << count;
+	cout << arr[N];
 }
