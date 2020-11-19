c775f11629816cc4a8c8a6298ed5be112e8d220e
rnrn213 <73531200+rnrn213@users.noreply.github.com>
2020-11-19 20:20:14 +0900

    6549번 히스토그램에서 가장 큰 직사각형(stack 사용 전)

--- 6549.cpp
+++ 6549.cpp
@@ -2,33 +2,61 @@
 
 using namespace std;
 
+long long getArea(const vector<long long>& v, int index) {
+	long long h = v[index];
+	long long area = h;
+	long long min = h;
+	bool decrease = false;
+	bool isRect = true;
+	if (h == 1) return 1;
+	int w = 1;
+	for (int i = index + 1; i < v.size(); i++) {
+		if (!isRect) return area;
+		w++;
+		if (v[i] >= h) {
+			if (decrease) isRect = false;
+			if (isRect) area = w * h;
+		}
+		else {
+			decrease = true;
+			if (min > v[i]) {
+				min = v[i];
+			}
+			else if (min < v[i]) {
+				isRect = false;
+			}
+
+			if (area < w * v[i] && isRect) {
+				area = w * v[i];
+			}
+		}
+	}
+	return area;
+}
+
 int main() {
 	cin.tie(NULL);
 	ios::sync_with_stdio(false);
 	
 	while (true) {
 		priority_queue<long long> pq;
+		vector<long long> v;
 		int n;
-		long long prevh, h, w;
 		cin >> n;
-		cout << "n = " << n << '\n';
 		if (n == 0) break;
-		
-		cin >> prevh;
-		w = 1;
-		for (int i = 0; i < n - 1; i++) {
+		long long max = n;
+
+		for (int i = 0; i < n; i++) {
+			long long h;
 			cin >> h;
-			if (prevh > h) {
-				pq.push(prevh * w);
-				cout << prevh * w << " pushed\n";
-				prevh = h;
-				w = 1;
-			}
-			else {
-				w++;
-			}
+			v.push_back(h);
 		}
-		pq.push(prevh * w);
-		cout << pq.top() << '\n';
+
+		for (int i = 0; i < v.size(); i++) {
+			long long area = getArea(v, i);
+			max = (max > area) ? max : area;
+		}
+		
+		cout << max << '\n';
 	}
 }
