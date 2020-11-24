15a39f53e811feff40e9a21c8ff4a212ce2e220a
rnrn213 <usan213@gmail.com>
2020-11-24 17:36:54 +0900

    2914번 저작권

---
+++ 2914.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+    int A, I;
+    cin >> A >> I;
+
+    cout << A * (I - 1) + 1;
+}
