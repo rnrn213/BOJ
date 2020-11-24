e0b1df1ae8ed701fdd11ef90cbdb49efab72d1ee
rnrn213 <usan213@gmail.com>
2020-11-24 17:49:14 +0900

    5338번 마이크로소프트 로고

---
+++ 5338.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+    cout << "       _.-;;-._\n'-..-'|   ||   |\n'-..-'|_.-;;-._|\n'-..-'|   ||   |\n'-..-'|_.-''-._|";
+}
