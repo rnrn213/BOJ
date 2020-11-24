37b167e4852fb33629a8a8c37b82174426396262
rnrn213 <73531200+rnrn213@users.noreply.github.com>
2020-11-24 19:31:26 +0900

    5339번 콜센터

---
+++ 5339.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+    cout << "     /~\\\n    ( oo|\n    _\\=/_\n   /  _  \\\n  //|/.\\|\\\\\n ||  \\ /  ||\n============\n|          |\n|          |\n|          |";
+}
