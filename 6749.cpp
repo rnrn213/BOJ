5aa34e4068a33a1b6fbba73868ca349e2dea858e
rnrn213 <73531200+rnrn213@users.noreply.github.com>
2020-11-26 23:49:46 +0900

    6749번 Next in line

---
+++ 6749.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+    int Y, M;
+    cin >> Y >> M;
+    cout << M + (M - Y);
+}
