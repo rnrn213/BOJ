6539a350ad50dbe98f98475f76cad292d2382eb3
rnrn213 <73531200+rnrn213@users.noreply.github.com>
2020-11-26 23:42:44 +0900

    8370번 Plane

---
+++ 8370.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+    int n1, k1, n2, k2;
+    cin >> n1 >> k1 >> n2 >> k2;
+    cout << n1 * k1 + n2 * k2;
+}
