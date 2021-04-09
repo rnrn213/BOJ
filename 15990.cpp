0e0349af2a86010aa06c4ef048d4fa78451684d2
rnrn213 <usan213@gmail.com>
2021-04-09 17:35:07 +0900

    15990번 1,2,3 더하기 5(solving)

---
+++ 15990.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int T, * n, cache[100001][4];
+vector<int> answers;
+
+void initCache() {
+    cache[1][0] = 1;
+    cache[1][1] = 0;
+    cache[1][2] = cache[1][3] = 1;
+
+    cache[2][0] = 1;
+    cache[2][2] = 0;
+    cache[2][1] = cache[2][3] = 1;
+
+    cache[3][0] = 3;
+    cache[3][1] = cache[3][2] = cache[3][3] = 2;
+}
+
+void input() {
+    cin >> T;
+    n = new int[T];
+    for (int i = 0; i < T; i++) {
+        cin >> n[i];
+    }
+}
+
+void solve() {
+    for (int i = 4; i <= 100000; i++) {
+        cache[i][0] = (cache[i - 1][1] + cache[i - 2][2] + cache[i - 3][3]) % 1000000009;
+        cache[i][1] = (cache[i - 2][2] + cache[i - 3][3]) % 1000000009;
+        cache[i][2] = (cache[i - 1][1] + cache[i - 3][3]) % 1000000009;
+        cache[i][3] = (cache[i - 1][1] + cache[i - 2][2]) % 1000000009;
+    }
+}
+
+void getAnswer() {
+    solve();
+    for (int i = 0; i < T; i++) {
+        answers.push_back(cache[n[i]][0]);
+    }
+}
+
+void output() {
+    for (int ans: answers) {
+        cout << ans << '\n';
+    }
+}
+
+int main() {
+    initCache();
+    input();
+    getAnswer();
+    output();
+}
