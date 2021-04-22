87f64f6f112fdfa16dc9eb562f756ee8905ec6c6
rnrn213 <73531200+rnrn213@users.noreply.github.com>
2021-04-22 23:58:01 +0900

    9465번 스티커

--- 9465.cpp
+++ 9465.cpp
@@ -6,7 +6,13 @@ int T, n, cache[2][100000];
 vector<int> stickers[2];
 
 void initCache() {
-    memset(cache, -1, sizeof(cache));
+    //memset(cache, -1, sizeof(cache));
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 100000; j++) {
+            cache[i][j] = -1;
+        }
+    }
 }
 
 void inputStickers() {
@@ -34,26 +40,18 @@ int solve(int line, int idx) {
     return ret;
 }
 
-void printCache() {
-    cout << "\ncache\n";
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << cache[i][j] << " ";
-        }
-        cout << '\n';
-    }
-}
-
 int main() {
     cin >> T;
     for (int i = 0; i < T; i++) {
         initCache();
         cin >> n;
         inputStickers();
-        cout << max(solve(0, 0), solve(1, 0));
+        for (int i = n - 1; i >= 1; i--) {
+            solve(0, i);
+            solve(1, i);
+        }
+        cout << max(solve(0, 0), solve(1, 0)) << '\n';
         stickers[0].clear();
         stickers[1].clear();
-
-        printCache();
     }
 }
