a1581b1922793d44b36ef6cd83dcbcb09b9ce540
rnrn213 <73531200+rnrn213@users.noreply.github.com>
2021-04-28 20:18:55 +0900

    15966번 군계일학(solving)

--- 15966.cpp
+++ 15966.cpp
@@ -16,15 +16,24 @@ void input() {
 
 int solve(int idx, int val) {
     if (idx == N) return 0;
-    
-    int ret = 0;
-    for (int i = idx; i < N; i++) {
-        if (arr[i] == val) ret = max(ret, 1 + solve(i + 1, val + 1));
+
+    int ret = 1;
+    for (int i = idx + 1; i < N; i++) {
+        if (arr[i] == val + 1) ret = max(ret, 1 + solve(i, arr[i]));
     }
     return ret;
 }
 
+int getAnswer() {
+    int ans = 1;
+
+    for (int i = 0; i < N; i++) {
+        ans = max(ans, solve(i, arr[i]));
+    }
+    return ans;
+}
+
 int main() {
     input();
-    cout << solve(0, 1);
+    cout << getAnswer();
 }
