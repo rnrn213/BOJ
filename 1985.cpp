c1b86b7f2d0ea9f98b69c0df377618b4a8f89107
rnrn213 <usan213@gmail.com>
2021-07-19 16:40:56 +0900

    1985번 디지털 친구

--- 1985.cpp
+++ 1985.cpp
@@ -2,7 +2,6 @@
 
 using namespace std;
 
-<<<<<<< HEAD
 vector<int> getNumbers(string n) {
     vector<int> ret;
     for (int i = 0; i < n.size(); i++) {
@@ -52,194 +51,20 @@ int main() {
             cout << "friends\n";
             continue;
         }
-        vector< vector<int> > saveNums = nums;
-
-        for (int j = 0; j < nums[0].size(); j++) {
-            auto it = find(nums[1].begin(), nums[1].end(), nums[0][j]);
-            if (it != nums[1].end()) {
-                nums[0].erase(nums[0].begin() + j);
-                j--;
-                nums[1].erase(it);
-            }
-        }
-
-        if (nums[0].size() != 0) {
-            bool isAlmostFriends = false;
-            for (int j = num[0].size() - 1; j > 0; j--) {
-                auto it = find(nums[0].begin(), nums[0].end(), num[0][j] - '0');
-                if (it != nums[0].end()) {
-                    vector<string> modifiedNums = modifyNum(num[0], j);
-                    for (int k = 0; k < modifiedNums.size(); k++) {
-                        if (saveNums[1] == getNumbers(modifiedNums[k]) && 
-                            modifiedNums[k][0] != '0') {
-                            isAlmostFriends = true;
-                            break;
-                        }
-                    }
-                    break;
-                }
-                it = find(nums[0].begin(), nums[0].end(), num[0][j - 1] - '0');
-                if (it != nums[0].end()) {
-                    vector<string> modifiedNums = modifyNum(num[0], j);
-                    for (int k = 0; k < modifiedNums.size(); k++) {
-                        if (saveNums[1] == getNumbers(modifiedNums[k]) && 
-                            modifiedNums[k][0] != '0') {
-                            isAlmostFriends = true;
-                            break;
-                        }
-                    }
-                    if (isAlmostFriends) break;
-                }
-
-            }
-            if (isAlmostFriends) {
-                cout << "almost friends\n";
-                continue;
-            }
-        }
-        if (nums[1].size() != 0) {
-            bool isAlmostFriends = false;
-            for (int j = num[1].size() - 1; j > 0; j--) {
-                auto it = find(nums[1].begin(), nums[1].end(), num[1][j] - '0');
-                if (it != nums[1].end()) {
-                    vector<string> modifiedNums = modifyNum(num[1], j);
-                    for (int k = 0; k < modifiedNums.size(); k++) {
-                        if (saveNums[0] == getNumbers(modifiedNums[k]) && 
-                            modifiedNums[k][0] != '0') {
-                            isAlmostFriends = true;
-                            break;
-                        }
-                    }
-                    break;
-                }
-                it = find(nums[1].begin(), nums[1].end(), num[1][j - 1] - '0');
-                if (it != nums[1].end()) {
-                    vector<string> modifiedNums = modifyNum(num[1], j);
-                    for (int k = 0; k < modifiedNums.size(); k++) {
-                        if (saveNums[0] == getNumbers(modifiedNums[k]) && 
-                            modifiedNums[k][0] != '0') {
-                            isAlmostFriends = true;
-                            break;
-                        }
-                    }
-                    if (isAlmostFriends) break;
-                }
-            }
-            if (isAlmostFriends) {
-                cout << "almost friends\n";
-                continue;
-            }
-        }
-        cout << "nothing\n";
-=======
-string modifyNum(string num, int idx1, int idx2, int value) {
-    num[idx1] += value;
-    num[idx2] -= value;
-
-    for (int i = idx1; i > 0; i--) {
-        if (num[i] - '0' == -1) {
-            num[i] = '9';
-            num[i - 1]--;
-        }
-        if (num[i] - '0' == 10) {
-            num[i] = '0';
-            num[i - 1]++;
-        }
-    }
-    if (num[0] - '0' == 10) {
-        num[0] = '0';
-        num = "1" + num;
-    }
-    return num;
-}
-
-string solveModified(string x, string y) {
-    for (int i = x.size() - 1; i >= 0; i--) {
-        if (y.find(x[i]) == string::npos) return "nothing";
-    }
-    return "almost friends";
-}
-
-string solve(string x, string y) {
-    string ret = "friends";
-    int changeNum[2] = {-1, 1};
-
-    if (x.size() == 1) {
-        if (y.find(x[0]) == string::npos) {
-            if (x[0] == '0') {
-                x[0]++;
-                if (y.find(x[0]) == string::npos) {
-                    ret = "nothing";
-                }
-                else {
-                    ret = "almost friends";
-                }
-            }
-            else if (x[0] - '0' >= 1 && x[0] - '0' < 9) {
-                for (int j = 0; j < 2; j++) {
-                    if (y.find(x[0] + changeNum[j]) == string::npos &&
-                        ret != "almost friends") {
-                        ret = "nothing";
-                    }
-                    else {
-                        ret = "almost friends";
+        
+        bool isAlmostFriends = false;
+        for (int j = 0; j < 2; j++) {
+            for (int k = num[j].size() - 1; k > 0; k--) {
+                vector<string> modifiedNums = modifyNum(num[j], k);
+                for (int n = 0; n < modifiedNums.size(); n++) {
+                    if (nums[!j] == getNumbers(modifiedNums[n]) && 
+                        modifiedNums[n][0] != '0') {
+                        isAlmostFriends = true;
                     }
                 }
             }
-            else {
-                string tempX = "10";
-                x[0]--;
-                for (int i = 0; i < tempX.size(); i++) {
-                    if (y.find(tempX[i]) == string::npos &&
-                        ret != "almost friends") {
-                        ret = "nothing";
-                    }
-                    else {
-                        ret = "almost friends";
-                    }
-                }
-                if (y.find(x[0]) == string::npos &&
-                    ret != "almost friends") {
-                    ret = "nothing";
-                }
-                else {
-                    ret = "almost friends";
-                }
-            }
         }
-        return ret;
-    }
-
-    for (int i = x.size() - 1; i > 0; i--) {
-        if (y.find(x[i]) == string::npos) {
-            for (int j = 0; j < 2; j++) {
-                string tempX = modifyNum(x, i, i - 1, changeNum[j]);
-                if (ret != "almost friends") ret = solveModified(tempX, y);
-                if (tempX[0] == '0') ret = "nothing";
-            }
-            return ret;
-        }
-        if (y.find(x[i - 1]) == string::npos) {
-            for (int j = 0; j < 2; j++) {
-                string tempX = modifyNum(x, i, i - 1, changeNum[j]);
-                if (ret != "almost friends") ret = solveModified(tempX, y);
-                if (tempX[0] == '0') ret = "nothing";
-            }
-            return ret;
-        }
-    }
-    return ret;
-}
-
-int main() {
-    string x, y, ans, solveX, solveY;
-    for (int i = 0; i < 3; i++) {
-        cin >> x >> y;
-        solveX = solve(x, y), solveY = solve(y, x);
-        if (solveX == "friends" || solveY == "friends") ans = "friends";
-        else if (solveX == "almost friends" || solveY == "almost friends") ans = "almost friends";
-        else ans = "nothing";
-        cout << ans << "\n";
->>>>>>> 8acdd685c1fb43161aad9f56e0fb62d275c8fd02
+        if (isAlmostFriends) cout << "almost friends\n";
+        else cout << "nothing\n";
     }
 }
