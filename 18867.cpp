da7dcdb9e2a4aff0b2a67f22c38a10348733f218
rnrn213 <73531200+rnrn213@users.noreply.github.com>
2021-10-05 22:51:13 +0900

    18867번 편지 꼭 해다오(solving)

--- 18867.cpp
+++ 18867.cpp
@@ -2,53 +2,59 @@
 
 using namespace std;
 
-vector<char> ans;
+string ans;
+vector<int> v;
 bool isFinished = false;
 
-//overflow 발생
-int s(int x, int index = 814) {
+long long s(int x, int index = 814) {
     if (index <= 4) {
-        return ((static_cast<int>(pow(x, index)) % 20200429) * (static_cast<int>(pow(x, index)) % 20200429)) % 20200429;
+        return ((static_cast<long long>(pow(x, index / 2)) % 20200429) * (static_cast<long long>(pow(x, index / 2 + index % 2)) % 20200429)) % 20200429;
     }
 
     return (s(x, index / 2) * s(x, index / 2 + index % 2)) % 20200429;
 }
 
-void solve(int y, vector<char>& v) {
+void getY(int y, int size) {
     if (y == 20200402) {
-        ans = v;
-        isFinished = true;
+        cout << "success";
         return;
     }
+    if (size > 990316) {
+        cout << y << "\n";
+        return;
+    }
+        
 
-    for (int i = 48; i <= 57; i++) {
-        if (!isFinished) {
-            v.push_back(i);
-            solve(((y % 20200429) + s(i)) % 20200429, v);
-            v.pop_back();
-        }
-    }
-    for (int i = 65; i <= 90; i++) {
-        if (!isFinished) {
-            v.push_back(i);
-            solve(((y % 20200429) + s(i)) % 20200429, v);
-            v.pop_back();
-        }
-    }
-    for (int i = 97; i <= 122; i++) {
-        if (!isFinished) {
-            v.push_back(i);
-            solve(((y % 20200429) + s(i)) % 20200429, v);
-            v.pop_back();
-        }
+    for (int i = 0; i < 62; i++) {
+        getY((y +v[i]) % 20200429, size + 1);
     }
 }
 
-int main() {
-    vector<char> v;
-    solve(0, v);
+void solve() {
 
-    for (auto c: ans) {
-        cout << c;
+    for (int i = 48; i <= 57; i++) {
+        v.push_back(s(i));
     }
+
+    for (int i = 65; i <= 90; i++) {
+        v.push_back(s(i));
+    }
+
+    for (int i = 97; i <= 122; i++) {
+        v.push_back(s(i));
+    }
+    
+    int y = 0;
+
+    while (y != 20200402) {
+        
+    }
+
+    getY(0, 0);
+}
+
+int main() {
+    solve();
+
+
 }
