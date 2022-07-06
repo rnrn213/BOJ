edbb86b33b472041bd9d534f324bb096cc597dd6
rnrn213 <73531200+rnrn213@users.noreply.github.com>
2022-07-07 03:24:56 +0900

    15657번 N과 M (8)

---
+++ 15657.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, M;
+vector<int> nums;
+
+void getProgression(vector<int>& v) {
+    if (v.size() == M) {
+        for (auto i: v) {
+            cout << nums[i] << " ";
+        }
+        cout << "\n";
+
+        return;
+    }
+
+    for (int i = *(v.end() - 1); i < N; i++) {
+        v.push_back(i);
+        getProgression(v);
+        v.pop_back();
+    }
+}
+
+int main() {
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) {
+        int num;
+        cin >> num;
+        nums.push_back(num);
+    }
+
+    sort(nums.begin(), nums.end());
+
+    for (int i = 0; i < N; i++) {
+        vector<int> v = {i};
+        getProgression(v);
+    }
+}
